730d72fae3ccb1be3d3fa932e7d1d1ef29b173d9
Niamul Hasan Zead <zead.ece2014@gmail.com>
2023-07-06 19:28:18 +0600

    selected product is showing results

--- src/PAGES/SelectedProduct/Selected.jsx
+++ src/PAGES/SelectedProduct/Selected.jsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
 import Navbar from '../Shared/Navbar';
 
 const Selected = () => {
+    const [others, setOthers] = useState([]);
     const { id } = useParams();
     const { data, isLoading } = useQuery({
         queryKey: ["product"],
@@ -15,12 +16,28 @@ const Selected = () => {
             return data;
         }
     });
+    const qCatagory = data?.catagory;
+    console.log(qCatagory);
+
+    useEffect(() => {
+        const url = `http://localhost:4000/products`;
+        fetch(url).then(res => res.json()).then(others => {
+            const otherData = others.filter(other => other.catagory !== qCatagory);
+            console.log(otherData);
+            setOthers(otherData);
+        });
+    }, [qCatagory])
 
     if (isLoading) {
         return <Loading></Loading>
     }
 
-    console.log(data);
+
+
+
+
+
+
 
     return (
         <div>
@@ -36,6 +53,23 @@ const Selected = () => {
                     </div>
                 </div>
             </div>
+            <div className='grid sm:grid-cols-1 lg:grid-cols-3 gap-2 mt-5'>
+                {
+                    others.map(other => <div key={other._id} className="card w-96 bg-base-100 shadow-xl">
+                        <figure className="px-10 pt-10">
+                            <img src={other.image} alt={other.name} className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">{other.name}</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>)
+                }
+            </div>
+
         </div>
     );
 };
